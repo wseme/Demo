9763566854da80e0fc73967926301881be41222f
Walter Seme <wsloth514@gmail.com>
2013-10-19 18:44:28 -0400

    Changed layout and updated Views

--- Gemfile
+++ Gemfile
@@ -2,11 +2,16 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Porsche 356 Registry" }
+
   describe "Home page" do
 
-	it "should have the right title" do
-  		visit '/'
-  		expect(page).to have_title("Porsche 356 Registry | Home")
-	end
-
-    it "should have the content 'Porsche 356 Registry'" do
+    it "should have the content 'Welcome'" do
       visit '/'
-      expect(page).to have_content('Porsche 356 Registry')
+      expect(page).to have_content('Welcome')
+    end
+
+    it "should have the base title" do
+      visit '/'
+      expect(page).to have_title("Porsche 356 Registry")
+    end
+
+    it "should not have a custom page title" do
+      visit '/'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -21,13 +28,41 @@ describe "Static pages" do
       visit '/static_pages/FAQs'
       expect(page).to have_content('FAQs')
     end
+
+  	it "should have the right title" do
+      	visit '/static_pages/FAQs'
+  		expect(page).to have_title("#{base_title} | FAQs")
+	end
+
+  end
+
+
+  describe "Fees page" do
+
+    it "should have the content 'Fees'" do
+      visit '/static_pages/Fees'
+      expect(page).to have_content('Fees')
+    end
+
+  	it "should have the right title" do
+      	visit '/static_pages/Fees'
+  		expect(page).to have_title("#{base_title} | Fees")
+	end
+
   end
 
   describe "Rules page" do
+
 
     it "should have the content 'Rules'" do
       visit '/static_pages/Rules'
       expect(page).to have_content('Rules')
     end
+
+    it "should have the right title" do
+      	visit '/static_pages/Rules'
+  		expect(page).to have_title("#{base_title} | Rules")
+	end
+
   end
 end
